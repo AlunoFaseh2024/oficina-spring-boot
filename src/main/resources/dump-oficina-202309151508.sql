d219ca0a125840c453757f8c6fedb9675a5f7ddd
AlunoFaseh2024 <163210335+AlunoFaseh2024@users.noreply.github.com>
2024-06-16 17:27:27 -0300

    Update dump-oficina-202309151508.sql
    Atualização do banco de dados

--- src/main/resources/dump-oficina-202309151508.sql
+++ src/main/resources/dump-oficina-202309151508.sql
@@ -1,10 +1,42 @@
-DROP TABLE IF EXISTS `tb_user`;
+-- --------------------------------------------------------
+-- Servidor:                     158.101.96.110
+-- Versão do servidor:           8.4.0 - MySQL Community Server - GPL
+-- OS do Servidor:               Linux
+-- HeidiSQL Versão:              12.7.0.6850
+-- --------------------------------------------------------
 
-CREATE TABLE `tb_user` (
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para oficina_db
+CREATE DATABASE IF NOT EXISTS `oficina_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `oficina_db`;
+
+-- Copiando estrutura para tabela oficina_db.tb_user
+CREATE TABLE IF NOT EXISTS `tb_user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela oficina_db.tb_user: ~3 rows (aproximadamente)
+INSERT IGNORE INTO `tb_user` (`id`, `nome`, `email`, `password`) VALUES
+	(1, 'Rafael Henrique Alves de Freitas', '972320116@ulife.com.br', 'rafa123'),
+	(3, 'Kaylaine Martins Cassimiro', 'kaylainecassimo20@gmail.com', 'kay123'),
+	(4, 'Wendel Victor Santos Xavier', '972310140@ulife.com.br', 'wendel123');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
